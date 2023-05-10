cbffbe1338715cda14e5f896bbe9299d6247f6aa
kapustakiszona <niahaibudze@gmail.com>
2023-05-10 14:55:42 +0200

    Base version. Map+ adding marker on tap

--- app/src/main/java/com/example/gatherersmap/MainActivity.kt
+++ app/src/main/java/com/example/gatherersmap/MainActivity.kt
@@ -3,21 +3,15 @@ package com.example.gatherersmap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.gatherersmap.presentation.ui.BottomSheet
-import com.example.gatherersmap.presentation.vm.MapViewModel
 import com.example.gatherersmap.ui.theme.GatherersMapTheme
-import com.google.android.gms.location.FusedLocationProviderClient
 
 class MainActivity : ComponentActivity() {
 
-
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private val viewModel: MapViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
